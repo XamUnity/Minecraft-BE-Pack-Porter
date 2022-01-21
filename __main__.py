fd388ca0f7e33511da81cd1bc54547dfdecbca08
Xam <97952123+XamUnity@users.noreply.github.com>
2022-01-21 18:38:32 +0800

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+from threading import Thread
+
+class Porter(Thread):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        while 1:
+            pass
+
+
+if __name__ == "__main__":
+    port = Porter()
+    port.start()
